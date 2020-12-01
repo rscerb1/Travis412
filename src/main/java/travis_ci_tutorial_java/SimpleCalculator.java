c49856a565666e419f664d91da173a4df3bb5e1e
rscerb1 <rscerb1@students.towson.edu>
2020-12-01 17:31:20 -0500

    added subtract, divide, and multiply methods

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -1,7 +1,8 @@
 package travis_ci_tutorial_java;
 
 public class SimpleCalculator {
-	public int add(int a, int b) {
-		return a + b;
-	}
+	public int add(int a, int b) {return a + b; }
+	public int subtract(int a, int b) {return a - b; }
+	public int divide(int a, int b) {return a / b; }
+	public int multiply(int a, int b) {return a * b; }
 }
--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -9,6 +9,9 @@ public class SimpleCalculatorTest {
 	@Test
 	public void testAdd() {
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.add(1, 1), 0);
+		assertEquals(calc.add(1, 1), 2);
+		assertEquals(calc.subtract(1, 1), 0);
+		assertEquals(calc.divide(10, 2), 5);
+		assertEquals(calc.multiply(2, 2), 4);
 	}
 }
